e3cbca5fdddbeccdb281da199d69d2924911a49c
Caaronbll <aaronbll46@gmail.com>
2024-12-10 15:11:06 -0600

    audio reader

--- Librosa_Animator/Audio_Analyze.py
+++ Librosa_Animator/Audio_Analyze.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 
 import numpy as np 
+import pandas as pd
+import matplotlib.pylab as plt
 import librosa
+import librosa.display
 print(librosa.__version__) # print version
+
+audio_path = 'Pj.mp3'
+y, sr = librosa.load(audio_path, sr=22050) # sr = sample rate(Hz)
+print(y[:300])
 
